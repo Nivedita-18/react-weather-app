02af718c3a687770cc8be72197e872a351fecea9
NIVEDITA <nibboyash@gmail.com>
2026-08-17 12:05:12 +0530

    Refactor useWeather hook
    Refactor useWeather hook by removing unused imports and simplifying state management.

--- src/hooks/useWeather.js
+++ src/hooks/useWeather.js
@@ -1,35 +1,11 @@
 import { useState } from "react";
-import {
-  getWeatherByCity,
-  getWeatherByCoords,
-} from "../services/weatherService";
-
-import { getBackground } from "../utils/weatherBackground";
 
 export function useWeather() {
-    
   const [city, setCity] = useState("");
   const [weather, setWeather] = useState(null);
   const [error, setError] = useState("");
-  const [loading, setLoading] = useState(false);
+  const [loading] = useState(false);
   const [background, setBackground] = useState("default");
-  const updateWeatherState = (data) => {
-
-  if (data.cod !== 200) {
-    setError(data.message);
-    setWeather(null);
-    return false;
-  }
-
-  setError("");
-  setWeather(data);
-
-  const condition = data.weather[0].main;
-  setBackground(getBackground(condition));
-
-  return true;
-
-};
 
   return {
     city,
@@ -38,5 +14,8 @@ export function useWeather() {
     error,
     loading,
     background,
+    setWeather,
+    setError,
+    setBackground,
   };
 }
